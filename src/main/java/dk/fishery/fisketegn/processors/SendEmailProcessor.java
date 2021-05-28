94ece821c7f059165f1fb9dbd97de928ca82c9b8
Mathias L. H <s144229@student.dtu.dk>
2021-05-28 13:32:40 +0200

    Expanded email sent when license bought.

--- src/main/java/dk/fishery/fisketegn/processors/SendEmailProcessor.java
+++ src/main/java/dk/fishery/fisketegn/processors/SendEmailProcessor.java
@@ -3,7 +3,10 @@ package dk.fishery.fisketegn.processors;
 import dk.fishery.fisketegn.model.User;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.util.json.JsonObject;
+import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -12,7 +15,11 @@ public class SendEmailProcessor implements Processor {
   @Override
   public void process(Exchange exchange) throws Exception {
     String to = (String) exchange.getProperty("userEmail");
-    String license = (String) exchange.getProperty("licenseNumber");
+    JsonObject license = (JsonObject) exchange.getProperty("license");
+
+    User user = (User) exchange.getProperty("oldBody");
+
+
     String from = "fisketegndtu@gmail.com";
     String password = (String) exchange.getProperty("emailPass");
 
@@ -35,8 +42,35 @@ public class SendEmailProcessor implements Processor {
       message.setFrom(new InternetAddress(from));
       message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
       message.setSubject("Tillykke med dit fisketegn");
-      message.setText("Dit fisketegnsnummer er " + license);
-      System.out.println("Dit fisketegnsnummer er " + license);
+      String text = "";
+      if(license.getString("type").equals("f")){
+        text = text + "Fritidsfisketegn\n";
+      }else{
+        text = text + "Lystfisketegn\n";
+      }
+      text = text + "Fiskerinummer   -   " + license.getString("licenseNumber") + "\n";
+      text = text + "Fiskekorttype   -   ";
+      if(license.getString("type").equals("f")){
+        text = text + "Fritidsfisker\n";
+      }else if(license.getString("type").equals("y")){
+        text = text + "Lystfisker - 1 år\n";
+      }else if(license.getString("type").equals("w")){
+        text = text + "Lystfisker - 1 uge\n";
+      }else if(license.getString("type").equals("d")){
+        text = text + "Lystfisker - 1 day\n";
+      }
+
+      text = text + "Fornavn   -   " + user.getFirstName() + "\n";
+      text = text + "Efternavn   -   " + user.getLastName() + "\n";
+      text = text + "Adresse   -   " + user.getAddress() + "\n";
+      text = text + "Postnummer   -    " + user.getZipCode() + "\n";
+      text = text + "Land    -    " + user.getCountry() + "\n";
+      text = text + "Startdate    -    " + license.getString("startDate") + "\n";
+      text = text + "Gyldigt til    -    " + license.getString("endDate") + "\n";
+
+
+      //System.out.println(text);
+      message.setText(text);
 
       Transport.send(message);
       System.out.println("Sent message successfully....");
@@ -44,4 +78,7 @@ public class SendEmailProcessor implements Processor {
       e.printStackTrace();
     }
   }
+
+  
+
 }
